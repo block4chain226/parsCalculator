d517dd0dafbc231c3f70b577b6752b5d3b64640f
Ivan <retouch226@gmail.com>
2021-12-29 17:40:43 +0200

    feat:add substract and division functions

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
 /*jshint esversion: 6 */
 /*jshint -W087 */
 let digit = 0;
-const exeption = "814560+232+2530*13334*62335436456/22-6/11*1243";
+const exeption = "810/232/2/1.5";
 let temp = 0;
 let result = 0;
 firstItteration = true;
 
 //check digits count function
-const checkDigit = (exeptionElement, elementIndex) => {
+const checkDigit = (exeptionElement) => {
   exeptionElement.forEach((element, index) => {
     if (
       index < exeptionElement.length - 1 &&
@@ -38,16 +38,52 @@ const checkDigit = (exeptionElement, elementIndex) => {
 const addition = (ex) => {
   const exeptionElements = checkDigit(exeption.split(""));
   for (let i = 0; i <= exeptionElements.length; i++) {
-    if (ex[i] === "+" && firstItteration === true) {
+    if (exeptionElements[i] === "+" && firstItteration === true) {
       temp = +exeptionElements[i - 1] + +exeptionElements[i + 1];
       result += temp;
       firstItteration = false;
       i++;
     }
-    if (ex[i] === "+" && firstItteration == false) {
+    if (exeptionElements[i] === "+" && firstItteration == false) {
       result += +exeptionElements[i + 1];
     }
   }
   console.log(`temp= ${temp}\n result= ${result}`);
 };
-addition(exeption);
+//addition(exeption);
+
+//Substruct function
+const substruct = (ex) => {
+  const exeptionElements = checkDigit(exeption.split(""));
+  for (let i = 0; i <= exeptionElements.length; i++) {
+    if (exeptionElements[i] === "-" && firstItteration === true) {
+      temp = +exeptionElements[i - 1] - +exeptionElements[i + 1];
+      result += temp;
+      firstItteration = false;
+      i++;
+    }
+    if (exeptionElements[i] === "-" && firstItteration == false) {
+      result -= +exeptionElements[i + 1];
+    }
+  }
+  console.log(`temp= ${temp}\n result= ${result}`);
+};
+//substruct(exeption);
+
+//division function
+const division = (ex) => {
+  const exeptionElements = checkDigit(exeption.split(""));
+  for (let i = 0; i <= exeptionElements.length; i++) {
+    if (exeptionElements[i] === "/" && firstItteration === true) {
+      temp = +exeptionElements[i - 1] / +exeptionElements[i + 1];
+      result = temp;
+      firstItteration = false;
+      i++;
+    }
+    if (exeptionElements[i] === "/" && firstItteration == false) {
+      result /= +exeptionElements[i + 1];
+    }
+  }
+  console.log(`temp= ${temp}\n result= ${result}`);
+};
+division(exeption);
